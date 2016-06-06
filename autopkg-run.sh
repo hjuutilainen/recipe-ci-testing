54a930db9de42eca2fb611eeda28fb2b61d4130d
Hannes Juutilainen <hjuutilainen@mac.com>
2016-06-06 20:56:56 +0300

    Modified run script

--- autopkg-run.sh
+++ autopkg-run.sh
@@ -2,6 +2,15 @@
 
 # Install AutoPkg
 git clone https://github.com/autopkg/autopkg.git
-cd autopkg && sudo ./Scripts/install.sh
+sudo ./autopkg/Scripts/install.sh
 
+# Add default recipes
 autopkg repo-add recipes
+
+# Add VirusTotalAnalyzer
+autopkg repo-add https://github.com/hjuutilainen/autopkg-virustotalanalyzer.git
+defaults write com.github.autopkg VIRUSTOTAL_AUTO_SUBMIT -bool true
+defaults write com.github.autopkg VIRUSTOTAL_SLEEP_SECONDS -int 5
+
+# Run recipes
+autopkg run -v --post "io.github.hjuutilainen.VirusTotalAnalyzer/VirusTotalAnalyzer" Firefox.download
